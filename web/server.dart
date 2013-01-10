b095030c401960c25b20e2b368cc9f851000886d
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-01-10 23:04:45 +0100

    Je réponds toujours OUI

--- web/server.dart
+++ web/server.dart
@@ -17,7 +17,8 @@ class DartStoryServer {
                             "Quelle est ton adresse email" : env["EMAIL"],
                             "Es tu abonne a la mailing list(OUI/NON)": env["MAILING_LIST"],
                             "Es tu heureux de participer(OUI/NON)" : "OUI",
-                            "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)":"OUI"
+                            "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)" : "OUI",
+                            "Est ce que tu reponds toujours oui(OUI/NON)" : "OUI"
   };
  
   final int port;
