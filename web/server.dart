51959352a76b2c4eebf81adf0ea29c309f2437ed
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-01-10 17:40:57 +0100

    ajout de unnitest

--- web/server.dart
+++ web/server.dart
@@ -1,20 +1,31 @@
-library dart_delivert_client;
+library dart_story_server;
 
 import "dart:io";
 
 var env = Platform.environment;
 
+HttpServer _server;
+
 main(){
+  startServer();
+}
+
+startServer(){
   print('Starting....');
   var port = env['PORT'] == null ? 12345 : int.parse(env['PORT']);
   var ip = "0.0.0.0";
 
-  var server = new HttpServer();
-  server.defaultRequestHandler = _serveHandler; 
-  server.listen(ip, port);
+  _server = new HttpServer();
+  _server.defaultRequestHandler = _serveHandler; 
+  _server.listen(ip, port);
 
 
   print('Listening for connections on $ip:$port');
+}
+
+stopServer(){
+  print("Stopping...");
+  _server.close();
 }
 
 _answer(HttpResponse response, String content){
