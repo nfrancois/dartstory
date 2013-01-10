4b2cf8834d78fd9c4b4e6f44ff2a9c467404dc6e
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-01-10 22:53:53 +0100

    pret pour le markdown

--- web/server.dart
+++ web/server.dart
@@ -16,7 +16,8 @@ class DartStoryServer {
   final Map queryAnswers = {
                             "Quelle est ton adresse email" : env["EMAIL"],
                             "Es tu abonne a la mailing list(OUI/NON)": env["MAILING_LIST"],
-                            "Es tu heureux de participer(OUI/NON)" : "OUI"
+                            "Es tu heureux de participer(OUI/NON)" : "OUI",
+                            "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)":"OUI"
   };
  
   final int port;
