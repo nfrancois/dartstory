5f7d4b9d3bc8e1d9b7e0734c302a5dce536075c5
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-01-12 20:31:11 +0100

    simple query test

--- test/operation_test.dart
+++ test/operation_test.dart
@@ -9,7 +9,9 @@ main(){
   group('Operation Tests', (){
     setUp(() => op = new Operation());
     test('1+1', add_1_and_1);
+    test('1+1', add_2_and_4);
     test('2*3', multiply_2_by_3);
+    test('1*5', multiply_1_by_5);
   });
 }
 
@@ -21,10 +23,27 @@ add_1_and_1(){
   expect(result, equals(2));
 }
 
+add_2_and_4(){
+  // When
+  var result = op.add(2, 4);
+  
+  // Then
+  expect(result, equals(6));
+}
+
+
 multiply_2_by_3(){
   // When
   var result = op.multiply(2, 3);
   
   // Then
   expect(result, equals(6)); 
+}
+
+multiply_1_by_5(){
+  // When
+  var result = op.multiply(1, 5);
+  
+  // Then
+  expect(result, equals(5)); 
 }
---
+++ test/simple_query_test.dart
@@ -0,0 +1,66 @@
+library opertion_test;
+
+import "package:unittest/unittest.dart";
+import "package:dart_story/query_analyser.dart";
+
+QueryAnalyser analyser;
+
+main(){
+  group("Simple query Tests", (){
+    setUp(() => analyser = new QueryAnalyser());
+    test("response email", should_reponse_email);
+    test("mailing list", should_be_abonne);
+    test("I am Happy", should_be_happy);
+    test("I am ready to reveive markdown", should_be_ready);
+    test("I don't always anwers OUI", should_not_always_response_oui);
+    test("Enonce 1 ok", should_have_receive_enonce1);
+  });
+}
+
+should_reponse_email(){
+  // When
+  var response = analyser.findAnswer("Quelle est ton adresse email");
+  
+  // Then
+  expect(response, equals("nicolas.franc@gmail.com"));
+}
+
+should_be_abonne(){
+  // When
+  var response = analyser.findAnswer("Es tu abonne a la mailing list(OUI/NON)");
+  
+  // Then
+  expect(response, equals("OUI"));
+}
+
+should_be_happy(){
+  // When
+  var response = analyser.findAnswer("Es tu heureux de participer(OUI/NON)");
+  
+  // Then
+  expect(response, equals("OUI"));  
+}
+
+should_be_ready(){
+  // When
+  var response = analyser.findAnswer("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)");
+  
+  // Then
+  expect(response, equals("OUI")); 
+}
+
+should_not_always_response_oui(){
+  // When
+  var response = analyser.findAnswer("Est ce que tu reponds toujours oui(OUI/NON)");
+  
+  // Then
+  expect(response, equals("NON")); 
+}
+
+should_have_receive_enonce1(){
+  // When
+  var response = analyser.findAnswer("As tu bien recu le premier enonce(OUI/NON)");
+  
+  // Then
+  expect(response, equals("OUI")); 
+}
